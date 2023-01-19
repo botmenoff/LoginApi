fc4fbf8295020854cf0d75100946afda846448a5
Ferran <marvenferry60@gmail.com>
2023-01-19 21:45:49 +0100

    login1.3.2

--- src/app.js
+++ src/app.js
@@ -39,7 +39,7 @@ app.use(morgan('dev'))
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     //Esto es una lista de las cabezeras permitidas en mi api
-    res.header('Access-Control-Allow-Headers', 'x-access-token');
+    res.header('Access-Control-Allow-Headers', '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Allow', 'GET, POST, PUT, DELETE');
     next();
